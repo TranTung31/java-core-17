1d0a632215746d113e92a7c95de7e80e0b983a96
Your Name <you@example.com>
2024-01-28 15:24:23 +0700

    Variables

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -3,5 +3,7 @@ package javacore;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		int result = 5 + 3 * 9 - 10 / 3;
+		System.out.println("result: " + result);
 	}
 }
