6f4c1b70b6e8145725b630605d14c944e6ce0971
Your Name <you@example.com>
2024-01-31 22:35:50 +0700

    docs: các hàm toán học

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Nhập tên của bạn: ");
-		String name = scanner.nextLine();
-		System.out.print("Nhập tuổi của bạn: ");
-		int age = scanner.nextInt();
-		System.out.println("Tên bạn là: " + name + ", tuổi là: " + age);
+		System.out.print("First number: ");
+		int firstNumber = scanner.nextInt();
+		System.out.print("Second number: ");
+		int secondNumber = scanner.nextInt();
+		System.out.println("Min: " + Math.min(firstNumber, secondNumber));
+		System.out.println("Pow: " + Math.pow(firstNumber, secondNumber));
 	}
 }
