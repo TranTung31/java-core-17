de8109f0cd53d579a133272f6df8ab9eb4132f09
Your Name <you@example.com>
2024-04-07 14:52:28 +0700

    docs: encapsulation (tính đóng gói)

--- src/oop/Encapsulation.java
+++ src/oop/Encapsulation.java
@@ -1,10 +1,12 @@
 package oop;
 
-public class InstanceConstructor {
+public class Encapsulation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student st = new Student("Nguyen Van B", 19); // constructor có tham số truyền vào
+		Student st = new Student("Nguyen Van C", 19);
+		st.setName("Nguyen Van D");
+		st.setAge(17);
 		System.out.println("Student with name = " + st.getName() + ", age = " + st.getAge());
 	}
 
